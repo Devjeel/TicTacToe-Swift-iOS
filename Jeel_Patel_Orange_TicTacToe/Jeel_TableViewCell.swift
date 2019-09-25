c1f080b8dc7971d80d922b0b45612ae21b9bbc18
Devjeel <jeelhp02@gmail.com>
2019-09-25 16:46:59 -0400

    Cells functionality added

---
+++ Jeel_Patel_Orange_TicTacToe/Jeel_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Jeel_TableViewCell.swift
+//  Jeel_Patel_Orange_TicTacToe
+//
+//  Created by Jeel Patel on 2019-09-25.
+//  Copyright © 2019 Jeel Patel. All rights reserved.
+//
+
+import UIKit
+
+class Jeel_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
